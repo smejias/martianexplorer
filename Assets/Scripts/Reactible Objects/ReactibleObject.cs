17c9bb31bb64dcb418fdc27bcb7fee342005f74b
pborjadeb <pabloborjaaa@gmail.com>
2018-09-19 00:12:23 -0300

    Refactor y mejoras en logica

---
+++ Assets/Scripts/Reactible Objects/ReactibleObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactibleObject : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void TurnOffOn(bool state)
+    {
+        gameObject.SetActive(state);
+    }
+
+    public virtual void Trigger()
+    {
+
+    }
+}
